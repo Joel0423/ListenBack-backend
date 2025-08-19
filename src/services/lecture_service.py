888269a4410d740810751e97a46d3916460b15fa
Joel Abhishek <joel.abhishek@mca.christuniversity.in>
2025-08-19 17:05:02 +0530

    added 5min timeout for firebase storage upload

--- src/services/lecture_service.py
+++ src/services/lecture_service.py
@@ -130,6 +130,6 @@ def upload_media_to_firebase(file_path, dest_blob_name):
     """
     bucket = storage.bucket()
     blob = bucket.blob(dest_blob_name)
-    blob.upload_from_filename(file_path)
+    blob.upload_from_filename(file_path, timeout=300)
     blob.make_public()
     return blob.public_url
